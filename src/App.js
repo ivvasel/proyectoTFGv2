0534a28d5c11ab52ddb7a59b46cf6d4fae8cc39f
ivvasel <ivvasel@teleco.upv.es>
2022-06-03 23:02:21 +0200

    Creando Auth Firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import { Home } from "./components/Home";
+import { Login } from "./components/Login";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // Inicializa la constante del usuario y le aplica el estado null
+  const {usuarioGlobal, setUsuarioGlobal} = useState(null); 
+
+    return <> {usuarioGlobal ? <Home /> : <Login/>} </>;
+    //Si usuarioGlobal existe abre el componente Home si no abre el componente Logueo.
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+//Crea la constante 'root' seleccionado el div con id = root
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//Renderiza el div seleccionado
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
